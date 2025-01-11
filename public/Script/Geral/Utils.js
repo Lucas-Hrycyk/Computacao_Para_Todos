b9c692555af4dde3c28fd2eba2425ee091437689
Lucas-Hrycyk <lucasantonio_02@hotmail.com>
2025-01-11 19:31:35 -0300

    - Mudança Utils

--- public/Script/Geral/Utils.js
+++ public/Script/Geral/Utils.js
@@ -1,34 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
   var menuDiv = document.getElementById('menu');
-  menuDiv.classList.add('hidden');
   
-  document.getElementById('toggleNavButton').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('hidden');
-  });
+  // Verifique se o menuDiv existe antes de adicionar a classe
+  if (menuDiv) {
+    menuDiv.classList.add('hidden');
+  }
+  
+  // Verifique se o toggleNavButton existe antes de adicionar o evento
+  var toggleNavButton = document.getElementById('toggleNavButton');
+  if (toggleNavButton) {
+    toggleNavButton.addEventListener('click', function() {
+      var menu = document.getElementById('menu');
+      if (menu) {
+        menu.classList.toggle('hidden');
+      }
+    });
+  }
 
   fetch('/Computacao_Para_Todos/public/Views/Geral/MenuPrincipal.html')
     .then(response => response.text())
     .then(data => {
-      document.getElementById('menu').innerHTML = data;
+      var menuElement = document.getElementById('menu');
+      if (menuElement) {
+        menuElement.innerHTML = data;
 
-      document.getElementById('toggleNavButtonPhone').addEventListener('click', function() {
-        var menu = document.getElementById('menu');
-        menu.classList.toggle('hidden');
-      });
+        // Verifique se o toggleNavButtonPhone existe antes de adicionar o evento
+        var toggleNavButtonPhone = document.getElementById('toggleNavButtonPhone');
+        if (toggleNavButtonPhone) {
+          toggleNavButtonPhone.addEventListener('click', function() {
+            menuElement.classList.toggle('hidden');
+          });
+        }
 
-      document.getElementById('FecharMenu').addEventListener('click', function() {
-        var menu = document.getElementById('menu');
-        menu.classList.add('hidden'); 
-      });
-
-
-
-    });
+        // Verifique se o FecharMenu existe antes de adicionar o evento
+        var fecharMenuButton = document.getElementById('FecharMenu');
+        if (fecharMenuButton) {
+          fecharMenuButton.addEventListener('click', function() {
+            menuElement.classList.add('hidden');
+          });
+        }
+      }
+    })
+    .catch(error => console.log('Erro ao carregar MenuPrincipal.html:', error));
 
   fetch('/Computacao_Para_Todos/public/Views/Geral/Footer.html')
     .then(response => response.text())
     .then(data => {
-      document.getElementById('footer').innerHTML = data;
-    });
+      var footerElement = document.getElementById('footer');
+      if (footerElement) {
+        footerElement.innerHTML = data;
+      }
+    })
+    .catch(error => console.log('Erro ao carregar Footer.html:', error));
 });
