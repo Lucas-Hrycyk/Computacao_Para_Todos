3c6323efe5873ff0d8673a9b7bb4090d2bcfd1e1
Lucas-Hrycyk <lucasantonio_02@hotmail.com>
2025-01-11 16:32:02 -0300

    - Correção DataBase

--- Config/dataBase.js
+++ Config/dataBase.js
@@ -1,14 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Definindo as credenciais de forma direta
+// Definindo as credenciais e configurando SSL
 module.exports = {
   username: 'lucas_hrycyk',
   password: 'y4tbBVtlL0WMGeqArw0O3X5yvz7ucDSO',
   database: 'computacao_todos_db',
   host: 'dpg-cu14u952ng1s73e6lqgg-a.oregon-postgres.render.com',
   port: 5432,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Necessário para evitar problemas com certificados não verificados
+    }
+  }
 };
 
 const database = new Sequelize({
@@ -17,7 +23,13 @@ const database = new Sequelize({
   database: 'computacao_todos_db',
   host: 'dpg-cu14u952ng1s73e6lqgg-a.oregon-postgres.render.com',
   port: 5432,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Necessário para aceitar certificados autoassinados
+    }
+  }
 });
 
 database.authenticate()
