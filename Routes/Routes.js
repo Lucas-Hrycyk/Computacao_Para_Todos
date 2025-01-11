f672c9d3c8b6216817950542775c0da891b83367
Lucas-Hrycyk <lucasantonio_02@hotmail.com>
2025-01-11 19:18:21 -0300

    - Criando a instancia express

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const route = express.Router();
+const app = express(); // Criação da instância do servidor Express
 
+// Serve os arquivos estáticos da pasta 'public' (CSS, JS, imagens, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota principal - Serve o index.html da pasta public
 route.get('/', (req, res) => {
-    // Serve o index.html da pasta public
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 //#region Mensagens
@@ -13,7 +17,10 @@ const controllerMensage = require('../Controllers/msgController')
 route.get('/ListMensagens', controllerMensage.List);
 route.post('/CreateMensagens', controllerMensage.Create);
 route.get('/getOneMensagens', controllerMensage.getOne)
-
 //#endregion
 
-module.exports = route;
+app.use(route); // Usa as rotas no servidor Express
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
