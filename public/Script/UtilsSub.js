28cc490a9ab2cde47115f661d092ad4fb3bc4b3a
Lucas-Hrycyk <lucasantonio_02@hotmail.com>
2025-01-11 21:00:46 -0300

    - Teste 3

---
+++ public/Script/UtilsSub.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuDiv = document.getElementById('menu');
+    
+    // Verifique se o menuDiv existe antes de adicionar a classe
+    if (menuDiv) {
+      menuDiv.classList.add('hidden');
+    }
+    
+    // Verifique se o toggleNavButton existe antes de adicionar o evento
+    var toggleNavButton = document.getElementById('toggleNavButton');
+    if (toggleNavButton) {
+      toggleNavButton.addEventListener('click', function() {
+        var menu = document.getElementById('menu');
+        if (menu) {
+          menu.classList.toggle('hidden');
+        }
+      });
+    }
+  
+    fetch('Views/Geral/MenuPrincipal.html')
+      .then(response => response.text())
+      .then(data => {
+        var menuElement = document.getElementById('menu');
+        if (menuElement) {
+          menuElement.innerHTML = data;
+  
+          // Verifique se o toggleNavButtonPhone existe antes de adicionar o evento
+          var toggleNavButtonPhone = document.getElementById('toggleNavButtonPhone');
+          if (toggleNavButtonPhone) {
+            toggleNavButtonPhone.addEventListener('click', function() {
+              menuElement.classList.toggle('hidden');
+            });
+          }
+  
+          // Verifique se o FecharMenu existe antes de adicionar o evento
+          var fecharMenuButton = document.getElementById('FecharMenu');
+          if (fecharMenuButton) {
+            fecharMenuButton.addEventListener('click', function() {
+              menuElement.classList.add('hidden');
+            });
+          }
+        }
+      })
+      .catch(error => console.log('Erro ao carregar MenuPrincipal.html:', error));
+  
+    fetch('Views/Geral/Footer.html')
+      .then(response => response.text())
+      .then(data => {
+        var footerElement = document.getElementById('footer');
+        if (footerElement) {
+          footerElement.innerHTML = data;
+        }
+      })
+      .catch(error => console.log('Erro ao carregar Footer.html:', error));
+  });
