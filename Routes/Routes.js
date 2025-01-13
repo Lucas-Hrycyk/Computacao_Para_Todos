f3c3874ee56cd2e30e7fee4247aba946a73d52c2
Lucas Antonio Alves Hrycyk <lucas.alves@almeidakruger.com.br>
2025-01-13 09:03:36 -0300

    - Fazendo a comunicação com o DataBase

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,21 +2,16 @@ const express = require('express');
 const path = require('path');
 const route = express.Router();
 
-// Serve arquivos estáticos da pasta 'public'
 route.use(express.static(path.join(__dirname, '../public')));
 
 route.get('/', (req, res) => {
-    // Serve o index.html da pasta public
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-//#region Mensagens
 const controllerMensage = require('../Controllers/msgController')
 
 route.get('/ListMensagens', controllerMensage.List);
 route.post('/CreateMensagens', controllerMensage.Create);
 route.get('/getOneMensagens', controllerMensage.getOne)
-
-//#endregion
 
 module.exports = route;
