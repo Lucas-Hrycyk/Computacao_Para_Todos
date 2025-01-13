e3da0e12c46aa0e2c69de8a65e15692dfdbdb63e
Lucas Antonio Alves Hrycyk <lucas.alves@almeidakruger.com.br>
2025-01-13 09:16:18 -0300

    - Corrigindo problema de comunicação com o banco de dados

--- public/Script/Index.js
+++ public/Script/Index.js
@@ -8,8 +8,7 @@ function abrirPopupForm() {
 
     comentarios.classList.add("desfocarFundo");
     mostrarForm.style.display = "block";
-  } 
-  else {
+  } else {
     console.error("Um ou mais elementos não foram encontrados.");
   }
 }
@@ -28,3 +27,42 @@ function fecharPopupForm() {
     console.error("Um ou mais elementos não foram encontrados.");
   }
 }
+
+function enviarMensagem(event) {
+  event.preventDefault();
+
+  var nomeUsuario = document.querySelector('input[type="text"]').value;
+  var mensagem = document.querySelector('textarea').value;
+
+  if (!nomeUsuario || !mensagem) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+
+  var dadosMensagem = {
+    UsuarioNome: nomeUsuario,
+    Mensagem: mensagem
+  };
+
+  console.log("Dados enviados: ", dadosMensagem);
+
+  fetch('/CreateMensagens', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dadosMensagem),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Mensagem enviada com sucesso:', data);
+    alert("Mensagem enviada com sucesso!");
+    fecharPopupForm();
+  })
+  .catch((error) => {
+    console.error('Erro ao enviar a mensagem:', error);
+    alert("Erro ao enviar mensagem. Tente novamente.");
+  });
+}
+
+document.getElementById("formEnviarMensagem").addEventListener("submit", enviarMensagem);
